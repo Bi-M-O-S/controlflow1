49e9fa25def7b82a4c3ddbc35f43a9f753914d73
Bi-M-O-S <sibuorbill@gmail.com>
2024-04-28 23:38:56 -0700

    This is my first push today.

---
+++ controlflow.dart
@@ -0,0 +1,13 @@
+userNun(int num) {
+  if (num > 10) {
+    print('Your number is greater than 10');
+  } else if (num == 10) {
+    print('Your number is equal to 10');
+  } else {
+    print('Your number is less than 10');
+  }
+}
+
+void main() {
+  userNun(40);
+}
